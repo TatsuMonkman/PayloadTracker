4c29b0098be1bf5283061598d31d22f1da7b3f5a
Tatsu Monkman <arukou47@gmail.com>
2018-04-02 08:38:55 -0700

    Minor styling changes to clean.py
    Made some minor styling changes to clean.py after reading the PEP 8
    style guid for python code.

--- Moon_Coords/clean.py
+++ Moon_Coords/clean.py
@@ -4,7 +4,9 @@ def clear(dat, tle):
     #set looks like [setn, setn + '_setup.txt', setn + '.bsp']
     import subprocess
 
-    subprocess.call('mv ' + tle + ' ./history/' + dat[0] + '.tle', shell = True)
+    subprocess.call('mv ' + tle + ' ./history/' + dat[0]
+                    + '.tle', shell = True)
     subprocess.call('mv ' + dat[1] + ' ./history/', shell = True)
     subprocess.call('mv ' + dat[2] + ' ./history/', shell = True)
-    subprocess.call('mv ' + dat[0] + '_spoints.txt ' + './history/', shell = True)
+    subprocess.call('mv ' + dat[0] + '_spoints.txt '
+                    + './history/', shell = True)
